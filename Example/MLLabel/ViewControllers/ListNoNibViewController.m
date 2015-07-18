9cbba28fc6fd05e6f7f2df90e3d8d5a5507830df
molon <dudl@qq.com>
2015-07-19 05:26:42 +0800

    same row count to using xib

--- Example/MLLabel/ViewControllers/ListNoNibViewController.m
+++ Example/MLLabel/ViewControllers/ListNoNibViewController.m
@@ -44,7 +44,7 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return 300;
+    return 30;
 }
 
 
