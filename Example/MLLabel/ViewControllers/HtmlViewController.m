a67fd7a209e34d09d87b201d3ecc9658039a7a58
molon <dudl@qq.com>
2015-08-28 10:06:25 +0800

    set font for html demo

--- Example/MLLabel/ViewControllers/HtmlViewController.m
+++ Example/MLLabel/ViewControllers/HtmlViewController.m
@@ -51,7 +51,7 @@
         SHOW_SIMPLE_TIPS(tips);
     }];
     
-    NSString* html = @"<center><b>黄莺儿</b></center><br/>"
+    NSString* html = @"<div style='font-size:14px'><center><b>黄莺儿</b></center><br/>"
     "<p>园林晴昼春谁主。<br/>暖律潜催，幽谷暄和，黄鹂翩翩，<font color='red'>乍迁芳树</font>。<br/>观露湿缕金衣，叶映如簧语。<br/>晓来枝上绵蛮，似把芳心、深意低诉。<sup>(1)</sup></p>"
     "<p>基本的html元素都支持，例如设置字体 <font face='Zapfino'>font</font>, 加粗"
     " <b>bold</b>, 斜体 <i>italics</i> 或者下划线 <u>underlined</u> <span style='color:red'>CSS</span> 等等。</p>"
@@ -60,7 +60,7 @@
     "<li>邮箱:&nbsp;<a href='mailto://dudl@qq.com'>我的邮箱</a></li>"
     "<li>电话:&nbsp;<a href='tel://13612341234'>假电话号码</a></li>"
     "</ul></p>"
-    "当然这些还是需要使用者自己尝试下，最好不要随便拿html源码使用，这里只是为了更好的利用html生成可用的NSAttributedString，仅仅作为辅助来用。";
+    "当然这些还是需要使用者自己尝试下，最好不要随便拿html源码使用，这里只是为了更好的利用html生成可用的NSAttributedString，仅仅作为辅助来用。</div>";
     
     //注意，默认转成的html会给链接+上下划线属性，我们这里可直接覆盖掉
     LABEL.linkTextAttributes = @{NSForegroundColorAttributeName:kDefaultLinkColorForMLLinkLabel,NSUnderlineStyleAttributeName:@(0)};
